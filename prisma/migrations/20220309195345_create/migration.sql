1d4756fbcda7b85f83cf2068132eb22e5e29019c
Guilherme <70561321+YuutaDragon@users.noreply.github.com>
2022-03-09 16:54:48 -0300

    refactor-  database config

--- prisma/migrations/20220309195345_create/migration.sql
+++ prisma/migrations/20220309195345_create/migration.sql
@@ -10,9 +10,8 @@ CREATE TABLE "users" (
 -- CreateTable
 CREATE TABLE "orders" (
     "id" TEXT NOT NULL,
+    "id_client" TEXT NOT NULL,
     "product_name" TEXT NOT NULL,
-    "origin" TEXT NOT NULL,
-    "destiny" TEXT NOT NULL,
     "width" DOUBLE PRECISION NOT NULL,
     "height" DOUBLE PRECISION NOT NULL,
     "length" DOUBLE PRECISION NOT NULL,
@@ -26,7 +25,7 @@ CREATE TABLE "orders" (
 CREATE TABLE "sugestions" (
     "id" TEXT NOT NULL,
     "id_deliveryman" TEXT NOT NULL,
-    "id_order" TEXT NOT NULL,
+    "id_delivery" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "sugestions_pkey" PRIMARY KEY ("id")
@@ -36,10 +35,12 @@ CREATE TABLE "sugestions" (
 CREATE TABLE "deliveries" (
     "id" TEXT NOT NULL,
     "id_client" TEXT NOT NULL,
-    "id_deliveryman" TEXT NOT NULL,
+    "id_deliveryman" TEXT,
     "id_order" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "status" TEXT NOT NULL,
+    "origin" TEXT NOT NULL,
+    "destiny" TEXT NOT NULL,
+    "price" DOUBLE PRECISION,
+    "status" TEXT NOT NULL DEFAULT E'available',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "update_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -50,16 +51,19 @@ CREATE TABLE "deliveries" (
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_id_client_fkey" FOREIGN KEY ("id_client") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "sugestions" ADD CONSTRAINT "sugestions_id_deliveryman_fkey" FOREIGN KEY ("id_deliveryman") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "sugestions" ADD CONSTRAINT "sugestions_id_order_fkey" FOREIGN KEY ("id_order") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "sugestions" ADD CONSTRAINT "sugestions_id_delivery_fkey" FOREIGN KEY ("id_delivery") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_id_client_fkey" FOREIGN KEY ("id_client") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_id_deliveryman_fkey" FOREIGN KEY ("id_deliveryman") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_id_deliveryman_fkey" FOREIGN KEY ("id_deliveryman") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_id_order_fkey" FOREIGN KEY ("id_order") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
