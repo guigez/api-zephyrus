f7e393eedc9e87bc22eef9a1c7a691e297874822
Guilherme <70561321+YuutaDragon@users.noreply.github.com>
2022-03-09 21:07:23 -0300

    Create - Add config cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 
 import "express-async-errors";
+
 import { AppError } from "./error/AppError";
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 app.use(routes);
